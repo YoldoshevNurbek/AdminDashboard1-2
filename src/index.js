be6416a7052d9a56dda8f3254a445829270987ce
YoldoshevNurbek <91670248+YoldoshevNurbek@users.noreply.github.com>
2021-12-04 12:42:36 +0500

    Update index.js
    sd

--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {Provider} from 'redux'
 ReactDOM.render(
-  <Provider>
   <React.StrictMode>
     <App />
   </React.StrictMode>
-  </Provider>,
   document.getElementById('root')
 );
 reportWebVitals();
